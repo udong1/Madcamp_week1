0f7ff60fae8ebdd20629852a73b57dbd4b71967b
tishakong <leehn45@naver.com>
2023-12-28 20:39:36 +0900

    Home -> Gallery로 수정
    Home -> Gallery로 수정

--- app/src/main/java/com/example/madcamp_week1/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/madcamp_week1/ui/gallery/GalleryViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.madcamp_week1.ui.home
+package com.example.madcamp_week1.ui.gallery
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Gallery Fragment"
     }
     val text: LiveData<String> = _text
 }
